b0bd8095437f851e3b98e93114bebe7eecb83590
Mr Horn <ahorn@mcsoxford.org>
2010-11-09 20:48:31 +0000

    Remove automatic truncation of RSS item titles because the client should control the presentation of RSS feeds.

--- src/main/java/org/mcsoxford/rss/RSSItem.java
+++ src/main/java/org/mcsoxford/rss/RSSItem.java
@@ -7,27 +7,7 @@ package org.mcsoxford.rss;
  */
 public class RSSItem extends RSSBase {
 
-  /**
-   * Limit the number of characters returned by {@link #toString()}.
-   */
-  private final int limit;
   private String category;
-
-  /**
-   * An RSS item with a custom limit.
-   * 
-   * @param limit number of characters displayed of the title
-   */
-  RSSItem(int limit) {
-    this.limit = limit;
-  }
-
-  /**
-   * An RSS item with a default title limit of 42 characters.
-   */
-  RSSItem() {
-    this(42);
-  }
 
   public String getCategory() {
     return category;
@@ -38,11 +18,10 @@ public class RSSItem extends RSSBase {
   }
 
   /**
-   * Returns the title of the RSS item. A title which exceeds the limit of
-   * characters is truncated with ellipses.
+   * Returns the title of the RSS item.
    */
   public String toString() {
-    return Strings.truncate(getTitle(), limit);
+    return getTitle();
   }
 
 }
