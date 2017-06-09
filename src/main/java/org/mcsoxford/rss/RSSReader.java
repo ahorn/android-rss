3aa584cf3bff23b5e6c50472362ba5658b0932d6
Nick <handnf@gmail.com>
2017-06-09 12:43:00 -0400

    Switch from Http Client to Http Url Connection
    This is required in newer versions of Android as Apache's Http Client is removed.

--- src/main/java/org/mcsoxford/rss/RSSReader.java
+++ src/main/java/org/mcsoxford/rss/RSSReader.java
@@ -18,126 +18,100 @@ package org.mcsoxford.rss;
 
 import java.io.IOException;
 import java.io.InputStream;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * HTTP client to retrieve and parse RSS 2.0 feeds. Callers must call
  * {@link RSSReader#close()} to release all resources.
- * 
+ *
  * @author Mr Horn
+ * @author Nick Felker (v1.1)
+ *
+ * @version 1.1 - Switch from Apache Http Client to Http Url Connection
  */
 public class RSSReader implements java.io.Closeable {
 
-  /**
-   * Thread-safe {@link HttpClient} implementation.
-   */
-  private final HttpClient httpclient;
+    /**
+     * Thread-safe RSS parser SPI.
+     */
+    private final RSSParserSPI parser;
 
-  /**
-   * Thread-safe RSS parser SPI.
-   */
-  private final RSSParserSPI parser;
-
-  /**
-   * Instantiate a thread-safe HTTP client to retrieve RSS feeds. The injected
-   * {@link HttpClient} implementation must be thread-safe.
-   * 
-   * @param httpclient thread-safe HTTP client implementation
-   * @param parser thread-safe RSS parser SPI implementation
-   */
-  public RSSReader(HttpClient httpclient, RSSParserSPI parser) {
-    this.httpclient = httpclient;
-    this.parser = parser;
-  }
-
-  /**
-   * Instantiate a thread-safe HTTP client to retrieve RSS feeds. The injected
-   * {@link HttpClient} implementation must be thread-safe. Internal memory
-   * consumption and load performance can be tweaked with {@link RSSConfig}.
-   * 
-   * @param httpclient thread-safe HTTP client implementation
-   * @param config RSS configuration
-   */
-  public RSSReader(HttpClient httpclient, RSSConfig config) {
-    this(httpclient, new RSSParser(config));
-  }
-
-  /**
-   * Instantiate a thread-safe HTTP client to retrieve and parse RSS feeds.
-   * Internal memory consumption and load performance can be tweaked with
-   * {@link RSSConfig}.
-   */
-  public RSSReader(RSSConfig config) {
-    this(new DefaultHttpClient(), new RSSParser(config));
-  }
-
-  /**
-   * Instantiate a thread-safe HTTP client to retrieve and parse RSS feeds.
-   * Default RSS configuration capacity values are used.
-   */
-  public RSSReader() {
-    this(new DefaultHttpClient(), new RSSParser(new RSSConfig()));
-  }
-
-  /**
-   * Send HTTP GET request and parse the XML response to construct an in-memory
-   * representation of an RSS 2.0 feed.
-   * 
-   * @param uri RSS 2.0 feed URI
-   * @return in-memory representation of downloaded RSS feed
-   * @throws RSSReaderException if RSS feed could not be retrieved because of
-   *           HTTP error
-   * @throws RSSFault if an unrecoverable IO error has occurred
-   */
-  public RSSFeed load(String uri) throws RSSReaderException {
-    final HttpGet httpget = new HttpGet(uri);
-
-    InputStream feedStream = null;
-    try {
-      // Send GET request to URI
-      final HttpResponse response = httpclient.execute(httpget);
-
-      // Check if server response is valid
-      final StatusLine status = response.getStatusLine();
-      if (status.getStatusCode() != HttpStatus.SC_OK) {
-        throw new RSSReaderException(status.getStatusCode(),
-            status.getReasonPhrase());
-      }
-
-      // Extract content stream from HTTP response
-      HttpEntity entity = response.getEntity();
-      feedStream = entity.getContent();
-
-      RSSFeed feed = parser.parse(feedStream);
-
-      if (feed.getLink() == null) {
-        feed.setLink(android.net.Uri.parse(uri));
-      }
-
-      return feed;
-    } catch (ClientProtocolException e) {
-      throw new RSSFault(e);
-    } catch (IOException e) {
-      throw new RSSFault(e);
-    } finally {
-      Resources.closeQuietly(feedStream);
+    /**
+     * Instantiate a thread-safe HTTP client to retrieve RSS feeds.
+     *
+     * @param parser thread-safe RSS parser SPI implementation
+     */
+    public RSSReader(RSSParserSPI parser) {
+        this.parser = parser;
     }
-  }
 
-  /**
-   * Release all HTTP client resources.
-   */
-  public void close() {
-    httpclient.getConnectionManager().shutdown();
-  }
+    /**
+     * Instantiate a thread-safe HTTP client to retrieve RSS feeds. Internal memory
+     * consumption and load performance can be tweaked with {@link RSSConfig}.
+     *
+     * @param config RSS configuration
+     */
+    public RSSReader(RSSConfig config) {
+        this(new RSSParser(config));
+    }
+
+    /**
+     * Instantiate a thread-safe HTTP client to retrieve and parse RSS feeds.
+     * Default RSS configuration capacity values are used.
+     */
+    public RSSReader() {
+        this(new RSSParser(new RSSConfig()));
+    }
+
+    /**
+     * Send HTTP GET request and parse the XML response to construct an in-memory
+     * representation of an RSS 2.0 feed.
+     *
+     * @param uri RSS 2.0 feed URI
+     * @return in-memory representation of downloaded RSS feed
+     * @throws RSSReaderException if RSS feed could not be retrieved because of
+     *           HTTP error
+     * @throws RSSFault if an unrecoverable IO error has occurred
+     */
+    public RSSFeed load(String uri) throws RSSReaderException {
+        InputStream feedStream = null;
+        try {
+            URL url = new URL(uri);
+            // Send GET request to URI
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoInput(true);
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            // Check if server response is valid
+            if (conn.getResponseCode() != 200) {
+                throw new RSSReaderException(conn.getResponseCode(),
+                        conn.getResponseMessage());
+            }
+
+            // Extract content stream from HTTP response
+            feedStream = conn.getInputStream();
+            RSSFeed feed = parser.parse(feedStream);
+
+            if (feed.getLink() == null) {
+                feed.setLink(android.net.Uri.parse(uri));
+            }
+
+            return feed;
+        } catch (IOException e) {
+            throw new RSSFault(e);
+        } finally {
+            Resources.closeQuietly(feedStream);
+        }
+    }
+
+    /**
+     * Release all HTTP client resources.
+     */
+    @Deprecated
+    public void close() {
+        // Don't need to close any HTTP clients.
+    }
 
 }
-
